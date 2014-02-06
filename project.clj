90106249f5eaa1397b39280369d6ba785afb612a
iamryancrooks <ryanc593@gmail.com>
2014-02-06 18:09:29 +0000

    Added csv reading depen

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
+                [clojure-csv/clojure-csv "2.0.1"]
   :main ^:skip-aot asthma.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
